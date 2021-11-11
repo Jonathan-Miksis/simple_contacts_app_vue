3df071580ad2cdbc9d3680d3ac3bfcd9b56b9e1a
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-10 22:07:04 -0500

    create boilerplate frontend app with vue

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for: contact for contacts
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        indexContacts: [],
+      };
+    },
+    created: function () {},
+    methods: {
+
+    },
+  };
+</script>
