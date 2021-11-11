a667a0b212bc8fbc0e7fa681a6e0a8326f62c87a
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-11 12:13:52 -0500

    create index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,38 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div v-for: contact for contacts
+    <div v-for="contact in contacts" v-bind:key="contact.id">
+      <p>First Name: {{ contact.first_name }}</p>
+      <p>Last Name: {{ contact.last_name }}</p>
+      <p>Email: {{ contact.email }}</p>
+      <p>Phone Number: {{ contact.phone_number }}</p>
+      <hr>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios';
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
-        indexContacts: [],
+        message: "Welcome to My Contacts",
+        contacts: [],
       };
     },
-    created: function () {},
-    methods: {
-
+    created: function () {
+      this.indexContacts();
     },
-  };
+    methods: {
+      indexContacts: function() {
+        console.log('getting the contacts from the API');
+        axios.get("/contacts").then(response => {
+        console.log(response.data);
+        this.contacts = response.data
+      });
+    },
+  },
+};
 </script>
